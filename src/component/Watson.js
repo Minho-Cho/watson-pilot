9588c0f9bd36da4bc767a849b2ab7d34ad039f89
necronia <necronia44@gmail.com>
2017-08-01 16:36:48 +0900

    오타수정

--- src/component/Watson.js
+++ src/component/Watson.js
@@ -6,4 +6,4 @@ export default class Watson extends React.Component {
             <div className="container"><img src="/images/logo.png" style={{width:"15px",height:"20px",position:"absolute",left:"20px"}}/><span style={{marginLeft:"30px"}}>{this.props.content}</span></div>
         );
     }
-}3
+}
